9ee05f477bef4802e4adbf8bc9a1f3d4a8b7292b
wzagrajcz <wzagrajcz@gmail.com>
2017-12-12 22:50:27 +0100

    Adding simple test and travis

---
+++ src/test/java/pl/agh/edu/iosr/microservices/users/SimpleTest.java
@@ -0,0 +1,13 @@
+package pl.agh.edu.iosr.microservices.users;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    public void someDummyTest() {
+        assertEquals("SSS", "SSS");
+    }
+}
